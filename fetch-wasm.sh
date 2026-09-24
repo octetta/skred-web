62d77a6a5cbdcf362ef5414a6c39e7e9ee66ac7f
Joseph Stewart <joseph.stewart@gmail.com>
2026-09-24 10:53:52 +0200

    Fix WASM extraction from zip archive

--- fetch-wasm.sh
+++ fetch-wasm.sh
@@ -2,6 +2,8 @@
 set -e
 VERSION=$(cat WASM_VERSION.txt)
 echo "Fetching version v${VERSION}..."
-curl -L -O "https://github.com/octetta/pulp/releases/download/v${VERSION}/skred_api.js"
-curl -L -O "https://github.com/octetta/pulp/releases/download/v${VERSION}/skred_api.wasm"
+curl -L -o wasm.zip "https://github.com/octetta/pulp/releases/download/v${VERSION}/skred-${VERSION}-maxed-wasm.zip"
+unzip -o wasm.zip "skred-${VERSION}-maxed/*"
+mv skred-${VERSION}-maxed/skred_api.* .
+rm -rf skred-${VERSION}-maxed wasm.zip
 echo "Done."
